f57880e2112b78e4c29b24dead570f1daed0e20b
vilayat-ali <vilayatcodemysite@gmail.com>
2025-01-12 17:05:27 +0530

    added new methods to DbModel trait and implemented them on player model

--- backend/src/common/src/traits/db_trait.rs
+++ backend/src/common/src/traits/db_trait.rs
@@ -1,8 +1,21 @@
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+pub enum ModelFieldValue {
+    IntVal(i32),
+    FloatVal(f32),
+    TextVal(String),
+    BoolVal(bool),
+}
+
 pub trait DBModel
 where
     Self: Clone + Send + Sync,
 {
     type ServiceError;
+    type Id;
+    type Schema;
+
     type CreatePayload;
     type CreateResponse;
 
@@ -13,4 +26,29 @@ where
     ) -> impl std::future::Future<Output = Result<Self::CreateResponse, Self::ServiceError>> + Send
     where
         Self::CreatePayload: std::marker::Send;
+
+    // get a record by id
+    fn read_by_id(
+        &self,
+        id: Self::Id,
+    ) -> impl std::future::Future<Output = Result<Self::Schema, Self::ServiceError>> + Send
+    where
+        Self::Id: std::marker::Send;
+
+    // update a record by id
+    fn update_by_id(
+        &self,
+        id: Self::Id,
+        update_data: HashMap<String, ModelFieldValue>,
+    ) -> impl std::future::Future<Output = Result<(), Self::ServiceError>> + Send
+    where
+        Self::Id: std::marker::Send;
+
+    // delete a record by id
+    fn delete_by_id(
+        &self,
+        id: Self::Id,
+    ) -> impl std::future::Future<Output = Result<(), Self::ServiceError>> + Send
+    where
+        Self::Id: std::marker::Send;
 }
